4d9abbeae659d395d9d15db5821657ce8ea914c6
hrsma2i <hrs.ma2i@gmail.com>
2019-05-08 15:46:04 +0900

    Make the tiny dataset of `fill_in_blank_test.json`

--- make_tiny.py
+++ make_tiny.py
@@ -17,7 +17,7 @@ def main():
         "train_no_dup.json",
         "valid_no_dup.json",
         "test_no_dup.json",
-        #"fill_in_blank_test.json",
+        "fill_in_blank_test.json",
     ]
     for json_filename in json_filenames:
         json_file = os.path.join(in_dir, json_filename)
@@ -37,13 +37,29 @@ def main():
         in_file = os.path.join(in_dir, filename)
         shutil.copy(in_file, out_dir)
 
-    print("Copy images in samples json files to tiny/labels")
+    print("Copy images used in sampled json files to tiny/images")
     in_img_dir = os.path.join(THIS_REPO, "main/images")
     out_img_dir = os.path.join(THIS_REPO, "tiny/images")
     for json_filename in json_filenames:
-        print("    Copy images in {}".format(json_filename))
+        print("    Copy images used in {}".format(json_filename))
         json_file = os.path.join(out_dir, json_filename)
-        set_ids = pd.read_json(json_file)["set_id"]
+        if json_filename == 'fill_in_blank_test.json':
+            set_ids = pd.read_json(json_file)['question']\
+                .apply(lambda item_ids: item_ids[0])\
+                .apply(lambda item_id: item_id.split('_')[0])\
+                .tolist()
+            # item_ids: [119704139_1, 119704139_3, 119704139_4, 119704...
+            # -> item_id: 119704139_1
+            # -> set_id: 119704139
+            set_ids += pd.read_json(json_file)['answers']\
+                .apply(lambda item_ids:\
+                    [item_id.split('_')[0] for item_id in item_ids])\
+                .sum()
+            # item_ids: [119704139_2, 148511719_1, 206598043_4, 106043...
+            # -> set_ids: [119704139, 148511719, 206598043, 106043...
+        else:
+            set_ids = pd.read_json(json_file)["set_id"]
+
         for set_id in set_ids:
             in_img_sub_dir = os.path.join(in_img_dir, str(set_id))
             out_img_sub_dir = os.path.join(out_img_dir, str(set_id))
